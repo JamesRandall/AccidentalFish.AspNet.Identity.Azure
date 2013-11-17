8b2699cd8ed7df5108b239b7e104dcf5e1cdd54b
James Randall <james@accidentalfish.com>
2013-11-17 16:57:18 +0000

    Removed dead usings

--- AccidentalFish.AspNet.Identity.Azure/TableUserClaim.cs
+++ AccidentalFish.AspNet.Identity.Azure/TableUserClaim.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Security.Claims;
-using System.Text;
-using System.Threading.Tasks;
-using Microsoft.WindowsAzure.Storage.Table;
+﻿using Microsoft.WindowsAzure.Storage.Table;
 
 namespace AccidentalFish.AspNet.Identity.Azure
 {
